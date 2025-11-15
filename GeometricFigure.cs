c8ec148e1edbdb2e8d80d42c5551e055c9c784d9
student-a502 <student-a502@STUD-RM20-A502>
2025-11-15 10:41:29 +0500

    Создание базового класса Баланова

---
+++ GeometricFigure.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace GeometricFigures
+{
+    public abstract class GeometricFigure
+    {
+        public Point Center { get; set; }
+
+        // Конструктор базового класса
+        protected GeometricFigure(double centerX, double centerY)
+        {
+            Center = new Point(centerX, centerY);
+        }
+
+        // Абстрактный метод для рисования
+        public abstract void Draw(System.Windows.Controls.Canvas canvas);
+    }
+}
+
